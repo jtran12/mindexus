53c80c5dcb9c73c964ddf6dd62133c7718efb390
jasonphan <jasphan@hotmail.com>
2015-11-03 17:43:53 -0500

    fixed logout in sign up page

--- mindexus/client/app/account/signup/signup.controller.js
+++ mindexus/client/app/account/signup/signup.controller.js
@@ -16,7 +16,7 @@ angular.module('mindexusApp')
         })
         .then( function() {
           // Account created, redirect to home
-          $location.path('/');
+          $location.path('/home');
         })
         .catch( function(err) {
           err = err.data;
